fe0f1dfa2985d7ad5c4b6e8fbc43bc36eb866ac3
Ryan Balfanz <balfanz@squareup.com>
2018-05-23 12:42:25 +0100

    Add initial portal index and URL patterns

--- cbpapp/urls.py
+++ cbpapp/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('app/', include('core.urls')),
+    path('portal/', include('portal.urls')),
     path('admin/', admin.site.urls),
 ]
