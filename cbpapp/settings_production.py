d93b08956412ff86486004495ce172b61999b16e
Ryan Balfanz <balfanz@squareup.com>
2018-05-23 14:08:34 +0100

    Add project apps to INSTALLED_APPS
    D’oh!

--- cbpapp/settings_production.py
+++ cbpapp/settings_production.py
@@ -3,3 +3,10 @@ from cbpapp.settings import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['cpbapp.herokuapp.com']
+
+PROJECT_APPS = [
+    'core',
+    'portal',
+]
+
+INSTALLED_APPS = INSTALLED_APPS + PROJECT_APPS
