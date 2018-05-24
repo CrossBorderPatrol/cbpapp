fd28b0c94131b141db360d569227cc0206efdb55
Ryan Balfanz <balfanz@squareup.com>
2018-05-24 09:52:54 +0100

    Add placeholders
    The code name ‘Reciprocity’ was chosen by Mark Hamlet.

--- portal/forms.py
+++ portal/forms.py
@@ -7,16 +7,25 @@ IDENTIFIER_CHOICES = (
 class SubjectOfInterestRequestForm(forms.Form):
     code_name = forms.CharField(
         required=False,
-        widget=forms.TextInput(attrs={ 'class': 'form-control' }),
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Reciprocity',
+        }),
     )
     name = forms.CharField(
         required=True,
         initial="JOHNIE BAUMKIRCHNER",
-        widget=forms.TextInput(attrs={ 'class': 'form-control' }),
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'JOHNIE BAUMKIRCHNER',
+        }),
     )
     address = forms.CharField(
         required=False,
-        widget=forms.TextInput(attrs={ 'class': 'form-control' }),
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': '10 Downing Street',
+        }),
     )
     #subject_identifier_type = forms.MultipleChoiceField(
     #    widget=forms.RadioSelect,
@@ -24,9 +33,15 @@ class SubjectOfInterestRequestForm(forms.Form):
     #)
     subject_identifier = forms.CharField(
         required=False,
-        widget=forms.TextInput(attrs={ 'class': 'form-control' }),
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'XXX-XX-XXXX',
+        }),
     )
     account = forms.CharField(
         required=False,
-        widget=forms.TextInput(attrs={ 'class': 'form-control' }),
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            # 'placeholder': 'Write your name here',
+        }),
     )
