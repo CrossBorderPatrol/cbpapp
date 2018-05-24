68dd682cb447e8660b28aec4f27f644eab375459
Ryan Balfanz <balfanz@squareup.com>
2018-05-24 09:18:58 +0100

    Bootstrap-ify the form a little bit

--- portal/forms.py
+++ portal/forms.py
@@ -5,12 +5,28 @@ IDENTIFIER_CHOICES = (
 )
 
 class SubjectOfInterestRequestForm(forms.Form):
-    code_name = forms.CharField(required=False)
-    name = forms.CharField(required=True, initial="JOHNIE BAUMKIRCHNER")
-    address = forms.CharField(required=False, widget=forms.Textarea)
+    code_name = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={ 'class': 'form-control' }),
+    )
+    name = forms.CharField(
+        required=True,
+        initial="JOHNIE BAUMKIRCHNER",
+        widget=forms.TextInput(attrs={ 'class': 'form-control' }),
+    )
+    address = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={ 'class': 'form-control' }),
+    )
     #subject_identifier_type = forms.MultipleChoiceField(
     #    widget=forms.RadioSelect,
     #    choices=IDENTIFIER_CHOICES,
     #)
-    subject_identifier = forms.CharField(required=False)
-    account = forms.CharField(required=False)
+    subject_identifier = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={ 'class': 'form-control' }),
+    )
+    account = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={ 'class': 'form-control' }),
+    )
