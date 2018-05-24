ce4ad4b0e46ecc80ad3fbc9e88ed15459e2e6f48
Ryan Balfanz <balfanz@squareup.com>
2018-05-24 09:25:49 +0100

    Fix duplicated url pattern name for SOI detail

--- portal/urls.py
+++ portal/urls.py
@@ -6,6 +6,6 @@ from portal.views import SubjectOfInterestRequestView
 urlpatterns = [
     path('', views.index, name='index'),
     path('subject-of-interest-request/', views.subject_of_interest_request, name='subject-of-interest-request-add'),
-    path('subject-of-interest-request/detail', views.subject_of_interest_request_detail, name='subject-of-interest-request-add'),
+    path('subject-of-interest-request/detail', views.subject_of_interest_request_detail, name='subject-of-interest-request-detail'),
     # path('subject-of-interest-request/add', SubjectOfInterestRequestView.as_view(), name='subject-of-interest-request-add'),
 ]
